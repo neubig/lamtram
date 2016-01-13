6877fb2def3d3e5b74e558cb6d7bf786788b24b3
Graham Neubig <neubig@gmail.com>
2016-01-12 20:00:42 -0500

    Made attention printing script support ensembles

--- script/print-attention.pl
+++ script/print-attention.pl
@@ -14,11 +14,10 @@ GetOptions(
 "lang=s" => \$LANG,
 );
 
-if(@ARGV < 1) {
-    print STDERR "Usage: $0 PREFIX [INTERLEAVE]\n";
+if(@ARGV != 1) {
+    print STDERR "Usage: $0 PREFIX\n";
     exit 1;
 }
-my $inter = $ARGV[1] ? $ARGV[1] : 1;
 
 my @vals = qw(0 1 2 3 4 5 6 7 8 9 A B C D E F);
 sub tohex {
@@ -28,24 +27,36 @@ sub tohex {
     return "$str$str$str";
 }
 
+sub create_context {
+  my @ret;
+  for(@_) {
+    my @arr = split(/,/, $_);
+    foreach my $i (0 .. $#arr) {
+      $ret[$i] += $arr[$i];
+    }
+  }
+  return map { $_/@_ } @ret;
+}
+
 my $id = 0;
 my ($src, $trg, @context);
 while(<STDIN>) {
     if(/^ll/) {
-        foreach my $fid (0 .. $inter-1) {
-            open FILE0, ">:utf8", "$ARGV[0]$id-$fid.html" or die "Couldn't open $ARGV[0]$id-$fid.html\n";
-            print FILE0 "<html><body><table>";
-            my @sa = split(/ +/, $src); push @sa, "&lt;s&gt;";
-            print FILE0 "<tr><td>&nbsp;</td>"; for(@sa) { print FILE0 "<td>$_</td>"; } print FILE0 "</tr>";
-            my @ta = split(/ +/, $trg); push @ta, "&lt;s&gt;";
-            foreach my $i (0 .. $#ta) {
-                print FILE0 "<tr><td>$ta[$i]</td>";
-                for(split(/,/, $context[$i*$inter+$fid])) { print FILE0 "<td bgcolor=\"".tohex($_)."\">&nbsp;</td>"; }
-                print FILE0 "<tr>";
-            }
-            print FILE0 "</table></body></html>";
-            close FILE0;
+        open FILE0, ">:utf8", "$ARGV[0]$id.html" or die "Couldn't open $ARGV[0]$id.html\n";
+        print FILE0 "<html><body><table>";
+        my @sa = split(/ +/, $src); push @sa, "&lt;s&gt;";
+        print FILE0 "<tr><td>&nbsp;</td>"; for(@sa) { print FILE0 "<td>$_</td>"; } print FILE0 "</tr>";
+        my @ta = split(/ +/, $trg); push @ta, "&lt;s&gt;";
+        if(@context % @ta != 0) { die "Context not divisible by ta: context=".scalar(@context).", ta=".scalar(@ta)."\n"; }
+        my $num_ens = @context / @ta;
+        foreach my $i (0 .. $#ta) {
+            print FILE0 "<tr><td>$ta[$i]</td>";
+            my @align = create_context($context[$i*$num_ens,($i+1)*$num_ens-1]);
+            for(@align) { print FILE0 "<td bgcolor=\"".tohex($_)."\">&nbsp;</td>"; }
+            print FILE0 "<tr>";
         }
+        print FILE0 "</table></body></html>";
+        close FILE0;
         @context = ();
         $id++;
     } elsif(/SentLL trg: (.*)/) {
