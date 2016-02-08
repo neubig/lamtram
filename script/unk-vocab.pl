89a04d731fabca605ace2e5a271234d54622e296
Graham Neubig <neubig@gmail.com>
2016-02-09 07:14:43 +0900

    Added ability to unk by vocab

---
+++ script/unk-vocab.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use utf8;
+use Getopt::Long;
+use List::Util qw(sum min max shuffle);
+binmode STDIN, ":utf8";
+binmode STDOUT, ":utf8";
+binmode STDERR, ":utf8";
+
+my $SYM = "<unk>"; # The symbol to use for unknown words
+GetOptions(
+"sym=s" => \$SYM,
+);
+
+if(@ARGV != 1) {
+    print STDERR "Usage: $0 VOCAB_FILE\n";
+    exit 1;
+}
+my %inv;
+open FILE0, "<:utf8", $ARGV[0] or die "Couldn't open $ARGV[0]\n";
+while(<FILE0>) {
+  chomp;
+  for(split(/ /)) {
+    $inv{$_}++;
+  }
+}
+
+while(<STDIN>) {
+  chomp;
+  my @arr = map { $inv{$_} ? $_ : $SYM } split(/ /);
+  print "@arr\n";
+}
